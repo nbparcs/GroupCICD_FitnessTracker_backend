e9fc9c8e6b88287126834d3896dca39fbd10e153
nathalie <nathalie@email.com>
2025-11-26 23:10:29 +1300

    1. add model register 2. add view register 3. add api register

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -16,3 +16,25 @@ class UserProfileSerializer(serializers.ModelSerializer):
                   'phone_number', 'date_of_birth', 'height', 'weight',
                   'gender', 'fitness_goal', 'created_at')
         read_only_fields = ('id', 'email', 'created_at')
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'password2', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
--- authentication/urls.py
+++ authentication/urls.py
@@ -3,12 +3,14 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     UserLoginView,
     UserLogoutView,
+    UserRegistrationView,
     UserProfileView
 )
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserProfileView.as_view(), name='profile'),
 ]
